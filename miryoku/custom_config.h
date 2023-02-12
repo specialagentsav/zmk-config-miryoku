c842407a78529b46ed9520469b5e3aa56bb3580a
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-02-12 19:05:00 +0100

    fun layer: swap pinky column with inner column (#18)

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -30,7 +30,7 @@ U_NP,              U_NP,              U_LT(U_MOUSE, ESC),U_LT(U_NAV, SPC),  U_LT
 U_NP,              U_NP,              &kp SEMICOLON,     &kp N0,            &kp COLON,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_LAYER_FUN \
-&kp F12,           &kp F9,            &kp F8,            &kp F7,            &kp PSCRN,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-&kp F11,           &kp F3,            &kp F2,            &kp F1,            &kp SLCK,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp F10,           &kp F6,            &kp F5,            &kp F4,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
+&kp SCLK,          &kp F9,            &kp F8,            &kp F7,            &kp F12,           U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp PSCRN,         &kp F3,            &kp F2,            &kp F1,            &kp F10,           U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp PAUSE_BREAK,   &kp F6,            &kp F5,            &kp F4,            &kp F11,           U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp TAB,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
