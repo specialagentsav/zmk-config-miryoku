d744e40cfeb524bbb5aac185e1e1260e7eb0f45a
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-03-05 21:59:56 +0100

    nav improvements on hold (#30)

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -44,10 +44,10 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPC),  U_LT
 U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp DEL,           &kp BSPC,          &kp RET,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_NAV \
-U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp INS,           &kp HOME,          &kp UP,            &kp END,           &kp PG_UP,         \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_CAPS_WORD_CAPS,  &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp PG_DN,         \
+U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp INS,           &kp HOME,          U_NAV_UP,          &kp END,           &kp PG_UP,         \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_CAPS_WORD_CAPS,  U_NAV_LEFT,        U_NAV_DOWN,        U_NAV_RIGHT,       &kp PG_DN,         \
 U_NA,              &kp RALT,          &u_to_U_SYM,       &u_to_U_NAV,       U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp DEL,           &kp BSPC,          &kp RET,           U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NAV_DEL,         U_NAV_BSPC,        &kp RET,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
 U_SINGLE_DOUBLE_TILDE,         U_SINGLE_TRIPLE_BACKTICK,     U_SINGLE_DOUBLE_BACKSLASH,             U_SINGLE_DOUBLE_AMPERSAND,                U_SINGLE_DOUBLE_PLUS,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
--- miryoku/hold_tap.h
+++ miryoku/hold_tap.h
@@ -6,3 +6,11 @@
 #define U_LEFT_RIGHT_PAREN HOLD_TAP(RPAR, LPAR)
 #define U_LEFT_RIGHT_BRACE HOLD_TAP(RIGHT_BRACE, LEFT_BRACE)
 #define U_LEFT_RIGHT_BRACKET HOLD_TAP(RIGHT_BRACKET, LEFT_BRACKET)
+
+// Inspired by https://github.com/urob/zmk-config
+#define U_NAV_LEFT  HOLD_TAP(HOME, LEFT)      // tap: left  | long-tap: beginning of line
+#define U_NAV_RIGHT HOLD_TAP(END, RIGHT)      // tap: right | long-tap: end of line
+#define U_NAV_UP    HOLD_TAP(LC(HOME), UP)    // tap: up    | long-tap: beginning of document
+#define U_NAV_DOWN  HOLD_TAP(LC(END), DOWN)   // tap: down  | long-tap: end of document
+#define U_NAV_BSPC  HOLD_TAP(LC(BSPC), BSPC)  // tap: bspc  | long-tap: delete word backward
+#define U_NAV_DEL   HOLD_TAP(LC(DEL), DEL)    // tap: del   | long-tap: delete word forward
