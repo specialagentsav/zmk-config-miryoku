a2d754c848dc13aed86ffc894d48232bfb1e8d9a
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-03-25 17:16:27 +0100

    refactor hold taps to include quick-tap-ms (#47)

--- miryoku/hold_tap.h
+++ miryoku/hold_tap.h
@@ -1,5 +1,5 @@
 // Copyright 2022 Lukas Kucera
-// https://github.com/kucera-lukas/miryoku
+// https://github.com/kucera-lukas/zmk-config
 
 #pragma once
 
@@ -8,12 +8,12 @@
 #define U_LEFT_RIGHT_BRACKET HOLD_TAP(RIGHT_BRACKET, LEFT_BRACKET)
 
 // Inspired by https://github.com/urob/zmk-config
-#define U_NAV_LEFT  HOLD_TAP(HOME, LEFT)      // tap: left  | long-tap: beginning of line
-#define U_NAV_RIGHT HOLD_TAP(END, RIGHT)      // tap: right | long-tap: end of line
-#define U_NAV_UP    HOLD_TAP(LC(HOME), UP)    // tap: up    | long-tap: beginning of document
-#define U_NAV_DOWN  HOLD_TAP(LC(END), DOWN)   // tap: down  | long-tap: end of document
-#define U_NAV_BSPC  HOLD_TAP(LC(BSPC), BSPC)  // tap: bspc  | long-tap: delete word backward
-#define U_NAV_DEL   HOLD_TAP(LC(DEL), DEL)    // tap: del   | long-tap: delete word forward
+#define U_NAV_LEFT  NAV_HOLD_TAP(HOME, LEFT)      // tap: left  | long-tap: beginning of line
+#define U_NAV_RIGHT NAV_HOLD_TAP(END, RIGHT)      // tap: right | long-tap: end of line
+#define U_NAV_UP    NAV_HOLD_TAP(LC(HOME), UP)    // tap: up    | long-tap: beginning of document
+#define U_NAV_DOWN  NAV_HOLD_TAP(LC(END), DOWN)   // tap: down  | long-tap: end of document
+#define U_NAV_BSPC  NAV_HOLD_TAP(LC(BSPC), BSPC)  // tap: bspc  | long-tap: delete word backward
+#define U_NAV_DEL   NAV_HOLD_TAP(LC(DEL), DEL)    // tap: del   | long-tap: delete word forward
 
 #define U_REFRESH_CLOSE_TAB HOLD_TAP(LC(W), LC(R))
 
---
+++ miryoku/mod_morph.h
@@ -0,0 +1,2 @@
+// Copyright 2022 Lukas Kucera
+// https://github.com/kucera-lukas/miryoku
